eb59a55cd23752d9882eb18f1c02ce5e370bb854
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-04-05 14:11:52 +0000

    Contact Form Component

--- src/app/components/contacts/contact-form/contact-form.component.ts
+++ src/app/components/contacts/contact-form/contact-form.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrl: './contact-form.component.css',
 })
 export class ContactFormComponent {
+  contactForm!: FormGroup;
 
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      active: new FormControl(true),
+    });
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  get phone() {
+    return this.contactForm.get('phone');
+  }
+
+  get active() {
+    return this.contactForm.get('active');
+  }
 }
