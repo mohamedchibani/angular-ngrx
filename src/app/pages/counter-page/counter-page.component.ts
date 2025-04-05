e8d0c4582ddaebafdc2bc96bde2c8aeb0d63e74d
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-04-05 09:21:49 +0000

    Use Selector in a Component

--- src/app/pages/counter-page/counter-page.component.ts
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -1,6 +1,9 @@
 import { AsyncPipe } from '@angular/common';
 import { CounterService } from './../../services/counter.service';
 import { Component, computed, effect, signal, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCount } from '../../store/counter/counter.selector';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter-page',
@@ -10,4 +13,11 @@ import { Component, computed, effect, signal, inject } from '@angular/core';
 })
 export class CounterPageComponent {
   counterService = inject(CounterService);
+  store = inject(Store);
+
+  count!: Observable<number>;
+
+  ngOnInit() {
+    this.count = this.store.select(selectCount);
+  }
 }
