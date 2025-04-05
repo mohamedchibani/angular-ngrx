845a9603876608b61868ef0c103f0f44e00ee3ce
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-04-05 14:41:32 +0000

    Display a Form

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { ContactGridComponent } from '../../components/contacts/contact-grid/contact-grid.component';
 import { ContactListComponent } from '../../components/contacts/contact-list/contact-list.component';
 import { AsyncPipe } from '@angular/common';
+import { ContactFormComponent } from '../../components/contacts/contact-form/contact-form.component';
 
 @Component({
   selector: 'app-contacts',
-  imports: [ContactGridComponent, ContactListComponent, AsyncPipe],
+  imports: [
+    ContactGridComponent,
+    ContactListComponent,
+    AsyncPipe,
+    ContactFormComponent,
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css',
 })
