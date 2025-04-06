e4417d295de157d602185ea78afe16b6d3fd16b6
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-04-06 19:01:58 +0100

    Create Contact Service

---
+++ src/app/services/contact.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ContactModel } from '../store/contact/contact.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactService {
+  apiUrl = 'http://localhost:3001/contacts';
+
+  http = inject(HttpClient);
+
+  loadAll(): Observable<ContactModel[]> {
+    return this.http.get<ContactModel[]>(this.apiUrl);
+  }
+
+  saveContact(contact: ContactModel): Observable<ContactModel> {
+    return this.http.post<ContactModel>(this.apiUrl, contact);
+  }
+
+  updateContact(contact: ContactModel, id: number): Observable<ContactModel> {
+    return this.http.put<ContactModel>(`${this.apiUrl}/${id}`, contact);
+  }
+
+  toggleStatus(active: boolean, id: number): Observable<ContactModel> {
+    return this.http.patch<ContactModel>(`${this.apiUrl}/${id}`, {
+      active: active,
+    });
+  }
+
+  destroy(id: number): Observable<null> {
+    return this.http.delete<null>(`${this.apiUrl}/${id}`);
+  }
+}
