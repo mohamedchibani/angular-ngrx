c0d33355e347c33225aa79c37af7a80f21263d72
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-04-09 10:22:13 +0100

    Router Selectors

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCurrentUrl } from '../../store/router/router.selector';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  store = inject(Store);
 
+  ngOnInit() {
+    this.store.select(selectCurrentUrl).subscribe((res) => console.log(res));
+  }
 }
