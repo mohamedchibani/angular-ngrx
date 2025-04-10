26895df9436033b22052ddb90c0b20deb172174c
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-04-10 21:49:23 +0100

    Category Form Validation

--- src/app/components/blog/category-form/category-form.component.ts
+++ src/app/components/blog/category-form/category-form.component.ts
@@ -1,10 +1,16 @@
 import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { CategoryStore } from '../../../store/category/category.store';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-category-form',
-  imports: [NgClass],
+  imports: [NgClass, ReactiveFormsModule],
   templateUrl: './category-form.component.html',
   styleUrl: './category-form.component.css',
 })
@@ -12,7 +18,19 @@ export class CategoryFormComponent {
   isOpen = false;
   categoryStore = inject(CategoryStore);
 
+  categoryForm!: FormGroup;
+
+  constructor() {
+    this.categoryForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    });
+  }
+
   ngOnInit() {
     this.isOpen = this.categoryStore.isOpen();
+  }
+
+  get name() {
+    return this.categoryForm.get('name');
   }
 }
